9094551db289aef91ead500f60744fdc9c614c9c
dmkorolev@yandex.ru <dmkorolev@yandex.ru>
2021-09-25 17:59:56 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/orphan/testmod
+
+go 1.17
---
+++ testmod.go
@@ -0,0 +1,10 @@
+package testmod
+
+import (
+	"fmt"
+	"github.com/orphan/test"
+)
+
+func Hi(name string) string {
+	return fmt.Sprintf("Hi, %s %s", name, test.Hello("roberto"))
+}
